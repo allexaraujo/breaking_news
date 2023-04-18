f9e1c10dcf2a2cd7ea95003346aac757fdbdd740
Állex Araujo <allexrodrigoa@gmail.com>
2023-04-18 15:21:52 -0300

    feat: added try/catch events on UserController

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,65 +1,78 @@
-const userService = require('../services/user.service')
-
+const userService = require('../services/user.service');
 
 const create = async (req, res) => {
-    const { name, username, email, password, avatar, background } = req.body;
+    try {
+        const { name, username, email, password, avatar, background } = req.body;
 
-    if (!name || !username || !email || !password || !avatar || !background) {
-        res.status(400).send({ message: "Submit all fields for registration" })
+        if (!name || !username || !email || !password || !avatar || !background) {
+            res.status(400).send({ message: "Submit all fields for registration" });
+        }
+
+        const user = await userService.create(req.body);
+
+        if (!user) {
+            return res.status(400).send({ messa: "Error creating Users" });
+        }
+
+        res.status(201).send({
+            message: "User created Successfully",
+            user
+        });
+    } catch (error) {
+        res.status(500).send({ message: `Internal Server Error: ${error.message}` });
     }
-
-    const user = await userService.create(req.body);
-
-    if (!user) {
-        return res.status(400).send({ messa: "Error creating Users" })
-    }
-
-    res.status(201).send({
-        message: "User created Successfully",
-        user
-    });
 
 }
 
 const findAll = async (req, res) => {
-    const users = await userService.findAll();
+    try {
+        const users = await userService.findAll();
 
-    if (users === 0) {
-        return res.status(404).send({ message: "This collection of users is empty" });
+        if (users === 0) {
+            return res.status(404).send({ message: "This collection of users is empty" });
+        }
+
+        res.send(users);
+    } catch (error) {
+        res.status(500).send({ message: `Internal Server Error: ${error.message}` });
     }
-
-    res.send(users);
 
 }
 
 const findById = async (req, res) => {
-
-    const { id, user } = req;
-    res.send(user);
+    try {
+        const { id, user } = req;
+        res.send(user);
+    } catch (error) {
+        res.status(500).send({ message: `Internal Server Error: ${error.message}` });
+    }
 
 }
 
 const update = async (req, res) => {
+    try {
+        const { name, username, email, password, avatar, background } = req.body;
 
-    const { name, username, email, password, avatar, background } = req.body;
+        if (!name && !username && !email && !password && !avatar && !background) {
+            res.status(400).send({ message: "Submit at least one field for update" });
+        }
 
-    if (!name && !username && !email && !password && !avatar && !background) {
-        res.status(400).send({ message: "Submit at least one field for update" })
+        const { id, user } = req;
+
+        await userService.update(
+            id,
+            name,
+            username,
+            email,
+            password,
+            avatar,
+            background
+        )
+
+        res.send({ message: "User successfully updated" });
+    } catch (error) {
+        res.status(500).send({ message: `Internal Server Error: ${error.message}` });
     }
-
-    const { id, user } = req;
-
-    await userService.update(
-        id,
-        name,
-        username,
-        email,
-        password,
-        avatar,
-        background
-    )
-
-    res.send({ message: "User successfully updated" })
 
 }
 
